1c3d10ef00f8d4714749d1ef4baf7800540d0a66
avelez94 <alante.v@gmail.com>
2026-08-13 11:56:50 -0400

    Widen coaching page containers to reduce side whitespace on large screens

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -81,7 +81,7 @@ export default function Nav() {
             {dropdownOpen && (
               <div className="nav-dropdown-menu" role="menu">
                 <Link href="/consulting" onClick={closeAll} role="menuitem">Leadership Consulting</Link>
-                <Link href="/coaching" onClick={closeAll} role="menuitem">Performance &amp; Developmental Coaching</Link>
+                <Link href="/coaching" onClick={closeAll} role="menuitem">Executive &amp; Leadership Coaching</Link>
                 <Link href="/parent-coaching" onClick={closeAll} role="menuitem">Coaching for Parents</Link>
                 <div className="nav-dropdown-divider" aria-hidden="true" />
                 <Link href="/impact" onClick={closeAll} role="menuitem">The Impact Lab</Link>
